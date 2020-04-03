2351262e6ebc423611ae9297e883d6f40dd46db3
Aakash Sangwan <47134609+Aakash2408@users.noreply.github.com>
2020-04-04 03:55:41 +0530

    Update Booking.vue

--- client/src/components/Booking.vue
+++ client/src/components/Booking.vue
@@ -8,8 +8,8 @@
        <div class="centered">
          
            <router-link to="/booking/pickup"><span class="dot">1</span><section class="text">Arrange Delivery and Pickup</section></router-link>
-          <router-link to="/booking/bbq"><span class="dot">2</span> <section class="text"> Choose your BBQ</section></router-link>
-            <router-link to="/booking/addons"><span class="dot">3</span><section class="text">Add ons</section></router-link>
+          <router-link to="/booking/product"><span class="dot">2</span> <section class="text"> Choose your Products</section></router-link>
+            
          
     
           <router-link to="/booking/map"><span class="dot">4</span> <section class="text"> Set up Drop-off point</section></router-link>
